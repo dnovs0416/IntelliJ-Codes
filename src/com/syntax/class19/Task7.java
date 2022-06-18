a9fb0c30054e888399736358a1e829cf5359936f
dnovs0416 <davenovales@gmail.com>
2022-06-18 15:30:13 -0400

    added class 19

---
+++ src/com/syntax/class19/Task7.java
@@ -0,0 +1,12 @@
+package com.syntax.class19;
+
+public class Task7 {
+
+    /* static Tas7(){
+
+     }*/
+    public static void main(String[] args) {
+        // Write program that have static constructor and observe result.
+    }
+
+}
